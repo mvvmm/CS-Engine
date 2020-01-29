11840b21bcad3a0201367ed55e65d591d4b456fc
MichaelvMorrison <morrim5@rpi.edu>
2020-01-29 15:53:05 -0500

    getting started with three js stuff

--- js/js.js
+++ js/js.js
@@ -1,23 +1,68 @@
-var canvas
+import * as THREE from '../node_modules/three/build/three.module.js';
 
-function setup(){
-  canvas = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+import Stats from '../node_modules/three/examples/jsm/libs/stats.module.js';
+import { GUI } from '../node_modules/three/examples/jsm/libs/dat.gui.module.js';
+
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+var renderer, scene, camera;
+
+var param = {};
+var stats;
+
+init();
+animate();
+
+function init() {
+
+  scene = new THREE.Scene();
+
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  document.body.appendChild( renderer.domElement );
+
+  // Check for float-RT support
+  // TODO (abelnation): figure out fall-back for float textures
+
+  if ( ! renderer.extensions.get( 'OES_texture_float' ) ) {
+
+    alert( 'OES_texture_float not supported' );
+    throw 'missing webgl extension';
+
+  }
+
+  if ( ! renderer.extensions.get( 'OES_texture_float_linear' ) ) {
+
+    alert( 'OES_texture_float_linear not supported' );
+    throw 'missing webgl extension';
+
+  }
+
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.set( 0, 20, 35 );
+
+  var controls = new OrbitControls( camera, renderer.domElement );
+  controls.minDistance = 20;
+  controls.maxDistance = 50;
+  controls.maxPolarAngle = Math.PI / 2;
+
+  scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+  var axesHelper = new THREE.AxesHelper( 100 );
+  scene.add( axesHelper );
+
+  var geometry = new THREE.PlaneBufferGeometry( 20, 20);
+  var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+  var plane = new THREE.Mesh( geometry, material );
+  scene.add( plane );
 }
 
-function draw(){
-  let number = $("#number").value;
-  background(250);
-  normalMaterial();
-  rectMode(CENTER);
-  translate(mouseX - width/2, mouseY -height/2);
-  rotateX(frameCount * 0.01);
-  box(width/25, width/25, width/25);
+function animate(){
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
 }
-
-window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
